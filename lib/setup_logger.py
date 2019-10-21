c1ea6c20c6f10f7a77de3fd0a6ca3b8a44c7a35f
Robert Schulz <schulz@astron.nl>
2019-10-21 15:22:44 +0200

    Added logger

---
+++ lib/setup_logger.py
@@ -0,0 +1,48 @@
+import logging
+import os
+import apersharp
+import subprocess
+
+
+def setup_logger(level="DEBUG", logfile=None):
+    """
+    Function to create a logging object for apersharp
+    """
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.propagate = False
+    logger.handlers = []
+
+    # set up handler
+    fh_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s',
+                                     datefmt='%m/%d/%Y %I:%M:%S %p')
+
+    if logfile is None:
+        fh = logging.FileHandler("Apersched.log")
+    else:
+        fh = logging.FileHandler(logfile)
+
+    fh.setLevel(logging.getLevelName(level))
+    fh.setFormatter(fh_formatter)
+    logger.addHandler(fh)
+
+    # set up output stream to command line
+    sh_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s : %(message)s',
+                                     datefmt='%m/%d/%Y %I:%M:%S %p')
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.getLevelName(level))
+    sh.setFormatter(sh_formatter)
+    logger.addHandler(sh)
+
+    logger.info("Logging started")
+
+    logger.info(
+        "Logging to file. To see the log in a bash window use the following command:")
+    logger.info("tail -n +1 -f {}".format(logfile))
+
+    gitinfo = subprocess.check_output('cd ' + os.path.dirname(apersharp.__file__) +
+                                      '&& git describe --tag; cd', shell=True).strip()
+    logger.info("Apersched version: {}".format(gitinfo))
+
+    return logger
