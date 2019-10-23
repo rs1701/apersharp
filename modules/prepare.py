7ffbbb511a9374faee2d6456c2218ab3122ff0d9
Robert Schulz <schulz@astron.nl>
2019-10-23 15:42:41 +0200

    Added function to create directories

--- modules/prepare.py
+++ modules/prepare.py
@@ -24,17 +24,48 @@ class prepare(BaseModule):
         Function to call all other function necessary to set things up
         """
 
-        # get the data
-
         # create the directory structure
+        self.set_directories()
+
+        # get the data
 
         # set things up for sharpener
 
         abort_function("Nothing here yet")
 
+    def set_directories(self):
+        """
+        Function to create the directory structure
+        """
+
+        # Create the cube directory
+        cube_subdir = "cube_{}".format(self.cube)
+        cube_dir = os.path.join(self.sharpener_basedir, cube_subdir)
+        if not os.path.exists(cube_dir):
+            try:
+                os.mkdir(cube_dir)
+            except Exception as e:
+                logger.exception(e)
+            else:
+                logger.info(
+                    "Cube {}: Created directory for cube".format(self.cube))
+
+        # Create beam directories if data is not coming from happili
+        if self.output_source != 'Happili':
+            for beam in range(self.NBEAMS):
+                beam_dir = os.path.join(cube_dir, "{:02d}".format(beam))
+                if not os.path.exists(beam_dir):
+                    try:
+                        os.mkdir(beam_dir)
+                    except Exception as e:
+                        logger.exception(e)
+                    else:
+                        logger.info(
+                            "Cube {0}: Created directory for beam {1}".format(self.cube, beam))
     def get_data(self):
         """
         Function to get the data depending on the source
+
         """
 
         beam_dir = np.arange(self.NBEAMS)
@@ -52,13 +83,28 @@ class prepare(BaseModule):
                 if os.path.exists(local_basedir):
                     logger.info(
                         "Found local directory for data {}".format(local_basedir))
-                    # copy the data
-                    abort_function(
-                        "Functionality to copy from a local data directory is not yet available")
+                    if local_basedir == self.sharpener_basedir:
+                        logger.info(
+                            "Data is already in the working directory.")
+                        get_data_local = True
+                    else:
+                        # copy the data
+                        abort_function(
+                            "Functionality to copy from a local data directory is not yet available")
             else:
                 logger.info("Data is already available")
-                get_data_local = True
         elif self.output_source == 'ALTA':
             if not get_data_alta:
+                # default path /altaZone/archive/apertif_main/visibilities_default/190409015_AP_B000
                 abort_function(
                     "Functionality to copy data from ALTA is not yet available")
+            else:
+                logger.info("Data is already available")
+        elif self.ouput_source == 'Happili':
+            if not get_data_happili:
+                abort_function(
+                    "Functionality to copy data from ALTA is not yet available")
+            else:
+                logger.info("Data is already available")
+
+        # store status parameters
