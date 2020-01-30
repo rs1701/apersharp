41c0037010def821ed4f5b1f203fd011400b6d15
Robert Schulz <schulz@astron.nl>
2020-01-30 10:34:01 +0100

    changed config file name again

--- lib/sharpener_pipeline.py
+++ lib/sharpener_pipeline.py
@@ -43,8 +43,8 @@ def sharpener_pipeline(beam_directory_list, do_source_finding, do_spectra_extrac
     # Load parameter file
     # +++++++++++++++++++
 
-    parameter_file = "{0:s}/sharpener_settings.yml".format(
-        beam_directory_list[beam_count])
+    parameter_file = "{0:s}/beam_{1}_sharpener_settings.yml".format(
+        beam_directory_list[beam_count], beam_name)
 
     if not os.path.exists(parameter_file):
         logger.info("(Pid {0:d}) ERROR: File {1:s} not found. Abort".format(
