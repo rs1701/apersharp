7b149b1e2f0d57b528c6746a19c8eca180025bdf
Robert Schulz <schulz@astron.nl>
2020-01-31 12:24:05 +0100

    removed debugging step

--- lib/sharpener_pipeline.py
+++ lib/sharpener_pipeline.py
@@ -87,8 +87,6 @@ def sharpener_pipeline(beam_directory_list, do_source_finding, do_spectra_extrac
             logger.info(
                 "(Pid {0:d}) ## Find continuum sources ... Done".format(proc))
 
-            raise RuntimeError("Stop here")
-
         # Extract spectra
         # +++++++++++++++
         if do_spectra_extraction:
