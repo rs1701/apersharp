9739f0fd99f1c5cf77a213bf700eae1e0604f26f
Robert Schulz <schulz@astron.nl>
2020-04-09 21:11:16 +0200

    added dtype command

--- lib/get_master_table.py
+++ lib/get_master_table.py
@@ -89,7 +89,7 @@ def get_all_sources_of_cube(output_file_name, cube_dir, taskid=None, cube_nr=Non
 
         # fix the column type
         src_data['FFLAG'] = np.array(["{}".format(flag)
-                                      for flag in src_data['FFLAG']])
+                                      for flag in src_data['FFLAG']], dtype=str)
 
         # create a new columns for ID, taskid, beam and cube
         src_id = np.array(["{0}_C{1}_B{2}_{3}_J{4}".format(
@@ -159,13 +159,8 @@ def get_all_sources_of_cube(output_file_name, cube_dir, taskid=None, cube_nr=Non
                         continue
 
                 # fix the column type
-                logger.debug(master_table['FFLAG'].dtype)
                 master_table['FFLAG'] = np.array(
                     ["{}".format(flag) for flag in master_table['FFLAG']], dtype=str)
-                logger.debug(master_table['FFLAG'].dtype)
-                logger.debug(np.array(
-                    ["{}".format(flag) for flag in master_table['FFLAG']]).dtype)
-                logger.debug(full_list['FFLAG'].dtype)
                 # combine master table with new data
                 full_list = vstack([master_table, full_list])
 
