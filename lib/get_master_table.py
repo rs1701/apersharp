a8773e51728bb8fd23f7d8290ed20de26f04a70d
Robert Schulz <schulz@astron.nl>
2020-04-09 21:01:14 +0200

    debug

--- lib/get_master_table.py
+++ lib/get_master_table.py
@@ -160,6 +160,8 @@ def get_all_sources_of_cube(output_file_name, cube_dir, taskid=None, cube_nr=Non
                 # fix the column type
                 master_table['FFLAG'] = np.array(
                     [str(flag) for flag in master_table['FFLAG']])
+                logger.debug(master_table['FFLAG'].dtype)
+                logger.debug(full_list['FFLAG'].dtype)
                 # combine master table with new data
                 full_list = vstack([master_table, full_list])
 
